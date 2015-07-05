04a5e81b6d3c15ea81733afbe120f5fe71942e75
Enzo Roiz <2161561R@student.gla.ac.uk>
2015-07-05 18:10:26 +0100

    Chapter 15 tests.

---
+++ tango_with_django_project/ch15tests/tests.py
@@ -0,0 +1,89 @@
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.urlresolvers import reverse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Create your tests here.
+class Chapter15ViewTests(TestCase):
+    def test_search_uses_template(self):
+        #Access search page
+        response = self.client.get('/rango/search/')
+
+        #Assert it uses search template
+        self.assertTemplateUsed(response, 'rango/search.html')
+
+    def test_search_form_is_displayed_correctly(self):
+        #Access search page
+        response = self.client.get('/rango/search/')
+
+        # Check form display
+        self.assertContains(response, '<input class="form-control" type="text" size="50" name="query"    '
+                                      'value="" id="query" />', html=True)
+
+        self.assertContains(response, '<input class="btn btn-primary"'
+                                      ' type="submit" name="submit" value="Search" />', html=True)
+
+
+    def test_search_url_mapping(self):
+        #Access search page
+        response = self.client.get(reverse('search'))
+
+        # Check that the rendered page was the search.html
+        self.assertEquals(response.request['PATH_INFO'], reverse('search'))
+
+    def test_nav_bar_contains_link_to_search(self):
+        #Access index page
+        response = self.client.get('/rango/')
+
+        #Check it has a link to search
+        self.assertContains(response, reverse('search'))
+
+    def test_result_list(self):
+        #Send search data via post
+        response = self.client.post('/rango/search/', {'query':'Django'})
+
+        # Get the result list
+        result_list = response.context['result_list']
+
+        # Assert there are 10 results displayed
+        self.assertEquals(len(result_list), 10)
+
+        # Check it has Django in the
+        for result in result_list:
+            self.assertIn('Django', result['title'])
+            self.assertIsNotNone(result['link'])
+            self.assertIsNotNone(result['summary'])
+
+class Chapter15LiveServerTestCase(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_users_search(self):
+        #Access index
+        self.browser.get(self.live_server_url + '/rango/')
+
+        #Click search
+        search_link = self.browser.find_element_by_link_text('Search')
+        search_link.click()
+
+        # Types and click search
+        search_field = self.browser.find_element_by_name('query')
+        search_field.send_keys('Django')
+        search_button = self.browser.find_element_by_name('submit')
+        search_button.click()
+
+        # Assert there will be a result heading
+        body_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Results', body_text)
+
+        # Assert there are 10 results displayed
+        result_items = self.browser.find_elements_by_class_name("list-group-item")
+        self.assertEquals(len(result_items), 10)
+
+
+
